7697f712bcc403bf0eff97470d1e48cba3bb5e27
maks-mishin <maks.mishinFZ@gmail.com>
2023-02-21 22:08:46 +0300

    Fix test_users assert

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,49 +2,47 @@ from urllib.parse import urljoin
 
 import requests
 
-BASE_URL = 'http://localhost:8080'
+BASE_URL = 'http://localhost:5000'
 
 
-def test_companies():
-    expected = [
-            {
-                'name': 'Wilson-Davis',
-                'phone': '001-011-530-0059'
-            },
-            {
-                'name': 'Gilbert-Hernandez',
-                'phone': '(108)731-8719'
-            },
-            {
-                'name': 'Davis, Riley and Adams',
-                'phone': '+1-147-543-3780'
-            },
-            {
-                'name': 'Jones, Cook and Mckinney',
-                'phone': '+1-620-197-2183x20518'
-            },
-            {
-                'name': 'Santiago Group',
-                'phone': '6402424270'
-            }
-        ]
-
-    response = requests.get(urljoin(BASE_URL, '/companies'))
-    assert response.json() == expected
+def test_has_form():
+    response = requests.get(urljoin(BASE_URL, '/users'))
+    assert 'form' in response.text
+    print('test_has_form passed')
 
 
-def test_companies_slice1():
-    expected = [
-        {'name': 'Davis, Riley and Adams', 'phone': '+1-147-543-3780'}
-    ]
-    response = requests.get(urljoin(BASE_URL, '/companies?page=3&per=1'))
-    assert response.json() == expected
+def test_users():
+    response = requests.get(urljoin(BASE_URL, '/users'))
+    assert 'Warren' in response.text
+    assert 'Amanda' in response.text
+    print('test_users passed')
 
 
-def test_companies_slice2():
-    expected = [
-        {'name': 'Thompson-Thompson', 'phone': '523-186-0248'},
-        {'name': 'Reed Inc', 'phone': '800-670-4239x790'}
-    ]
-    response = requests.get(urljoin(BASE_URL, '/companies?page=20&per=2'))
-    assert response.json() == expected
+def test_starts_with_term():
+    response = requests.get(urljoin(BASE_URL, '/users?term=al'))
+    assert 'Alyssa' in response.text
+    assert 'Alexa' in response.text
+    assert 'Allison' in response.text
+    assert 'Sarah' not in response.text
+    print('test_starts_with_term passed')
+
+
+def test_with_term_in_middle():
+    response = requests.get(urljoin(BASE_URL, '/users?term=al'))
+    assert 'Gerald' not in response.text
+    assert 'Randall' not in response.text
+    print('test_with_term_in_middle passed')
+
+
+def test_not_found_term():
+    response = requests.get(urljoin(BASE_URL, '/users?term=aaaaa'))
+    assert 'aaaaa' in response.text
+    print('test_not_found_term passed')
+
+
+if __name__ == '__main__':
+    test_has_form()
+    test_users()
+    test_with_term_in_middle()
+    test_not_found_term()
+    test_starts_with_term()
